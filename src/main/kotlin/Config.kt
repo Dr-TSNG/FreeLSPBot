fa2e29763291e4a9a6c2b722c684082003dab15f
Nullptr <52071314+Dr-TSNG@users.noreply.github.com>
2022-06-22 13:39:15 +0800

    Default non-anonymous

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -2,8 +2,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Config(
+    val proxyHost: String,
+    val proxyPort: Int,
     val token: String,
     val groupWhiteList: Set<Long>,
-    val proxyHost: String,
-    val proxyPort: Int
+    val verifyTimeout: Int,
+    val verifyFailedBanTime: Int,
 )
